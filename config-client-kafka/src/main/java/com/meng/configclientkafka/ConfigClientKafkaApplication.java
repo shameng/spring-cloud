2c4cb99024cf233ce01d2448384e7f305ca76bdc
shameng <879163994@qq.com>
2017-12-25 17:22:14 +0800

    kafka

---
+++ config-client-kafka/src/main/java/com/meng/configclientkafka/ConfigClientKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.meng.configclientkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientKafkaApplication.class, args);
+	}
+}
---
+++ hello-config-kafka/src/main/java/com/meng/helloconfigkafka/HelloConfigKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.meng.helloconfigkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloConfigKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloConfigKafkaApplication.class, args);
+	}
+}
