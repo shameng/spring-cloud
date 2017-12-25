5b91e61d60642dabf5a40e2f37808b16852c7671
shameng <879163994@qq.com>
2017-12-25 11:34:01 +0800

    mq

---
+++ hello-rabbitmq/src/main/java/com/meng/hellorabbitmq/HelloRabbitmqApplication.java
@@ -0,0 +1,12 @@
+package com.meng.hellorabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRabbitmqApplication.class, args);
+	}
+}
