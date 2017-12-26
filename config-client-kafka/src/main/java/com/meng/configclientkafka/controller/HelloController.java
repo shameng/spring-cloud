9aa23eeb769a0c74564700103e1b5890626ca130
imagedt_xindemeng <dingbang@imagedt.com>
2017-12-26 20:13:00 +0800

    *

---
+++ config-client-kafka/src/main/java/com/meng/configclientkafka/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.meng.configclientkafka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xindemeng
+ * @datetime 2017/12/23 15:01
+ */
+@RefreshScope
+@RestController
+public class HelloController {
+
+    @Value("${from}")
+    private String from;
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from + "--- " + env.getProperty("from", "undefined");
+    }
+}
